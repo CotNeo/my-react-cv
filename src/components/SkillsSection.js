5261c0fabc0a4ee407f05a06043b1b918afb1d76
Furkan AKAR <120303878+CotNeo@users.noreply.github.com>
2025-02-24 13:00:08 +0300

    Update SkillsSection.js

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -6,43 +6,43 @@ function SkillsSection() {
       <h2>Skills</h2>
       <div className="skills-container">
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/mongodb.png`} alt="MongoDB Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB Logo" />
           <p>MongoDB</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/expressjs.png`} alt="Express.js Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="Express.js Logo" />
           <p>Express.js</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/reactjs.png`} alt="React Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="React Logo" />
           <p>React</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/nodejs.png`} alt="Node.js Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="Node.js Logo" />
           <p>Node.js</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/bootstrap.png`} alt="Bootstrap Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg" alt="Bootstrap Logo" />
           <p>Bootstrap</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/html5.png`} alt="HTML5 Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="HTML5 Logo" />
           <p>HTML5</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/css3.png`} alt="CSS3 Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="CSS3 Logo" />
           <p>CSS3</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/api.png`} alt="API Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/api/api-original.svg" alt="API Logo" />
           <p>API</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/jvt.png`} alt="JWT Token Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jwt/jwt-original.svg" alt="JWT Token Logo" />
           <p>JWT Token</p>
         </div>
         <div className="skill">
-        <img src={`${process.env.PUBLIC_URL}/images/socketio.png`} alt="Socket.io Logo" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/socketio/socketio-original.svg" alt="Socket.io Logo" />
           <p>Socket.io</p>
         </div>
       </div>
